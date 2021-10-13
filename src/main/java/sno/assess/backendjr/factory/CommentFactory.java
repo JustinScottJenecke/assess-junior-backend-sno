a3dcdebd02758994b214bb0f53c9d4e80d900e59
Justin <justinjenecke@gmail.com>
2021-10-13 09:57:50 +0200

    populates CommentFactory.java with code and methods to create new intances of Comment.java

--- src/main/java/sno/assess/backendjr/factory/CommentFactory.java
+++ src/main/java/sno/assess/backendjr/factory/CommentFactory.java
@@ -6,5 +6,20 @@ package sno.assess.backendjr.factory;
  * Date: 13/10/2021
  * */
 
+import sno.assess.backendjr.entity.Comment;
+import sno.assess.backendjr.entity.Post;
+import sno.assess.backendjr.entity.User;
+
 public class CommentFactory {
+
+    public static Comment createComment(String body,
+                                        User author,
+                                        Post post) {
+
+        return new Comment.Builder()
+                .setBody(body)
+                .setUser(author)
+                .setPost(post)
+                .build();
+    }
 }
